57445b8adc231cd50f987ddf7a972a2e62f2d56b
sivaawsguygit1 <117250945+sivaawsguygit1@users.noreply.github.com>
2023-03-22 08:10:52 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,8 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World ***1******");
+        System.out.println("Hello, World ***2******");
     }
 }
